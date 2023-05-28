c9b2ca485f8467a7d28e0522c9551ace5b774ed4
Luis Ramírez Calle <luis.ramirezcalle@outlook.com>
2023-05-28 19:56:18 +0100

    Translate to spanish and fix linter

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -7,6 +7,8 @@ function withOpacity(variableName) {
   };
 }
 
+const { fontFamily } = require('tailwindcss/defaultTheme')
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
@@ -57,7 +59,7 @@ module.exports = {
       transparent: "transparent",
     },
     fontFamily: {
-      mono: ["IBM Plex Mono", "monospace"],
+      mono: ["IBM Plex Mono", "monospace", ...fontFamily.mono],
     },
     // },
   },
